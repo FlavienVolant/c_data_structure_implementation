8f2e57018cf48b53b868c088efda9ad1a9c15d41
Volant <flavienvolant@gmail.com>
2025-11-22 14:20:22 +0100

    find_value_after_insert test pass

--- binary_search_tree/test/bst_test.c
+++ binary_search_tree/test/bst_test.c
@@ -55,6 +55,25 @@ int find_value_in_a_just_create_bst_should_find_the_root_specified(void *params)
     return 0;
 }
 
+int find_value_after_insert(void *params) {
+    BST_t *tree = params;
+    int res;
+    
+    int keys[] = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+    int values[] = {1, 4, 8, 16, 32, 64, 128, 256, 512};
+
+    for(int i = 0; i < sizeof(keys)/sizeof(int); i ++) {
+        insert(tree, keys[i], values[i]);
+    } 
+    
+    for(int i = 0; i < sizeof(keys)/sizeof(int); i ++) {
+        ASSERT_TRUE(find_value(tree, keys[i], &res) == 0);
+        ASSERT_EQUALS(res, values[i]);
+    }
+
+    return 0;
+}
+
 int main() {
     struct TestList *tests = create_test_list();
 
@@ -62,6 +81,7 @@ int main() {
     ADD_TEST(tests, insert_lower_key_value_than_root_should_create_a_subtree_left);
     ADD_TEST(tests, insert_higher_key_value_than_root_should_create_a_subtree_right);
     ADD_TEST(tests, find_value_in_a_just_create_bst_should_find_the_root_specified);
+    ADD_TEST(tests, find_value_after_insert);
 
     run_tests(tests, beforeEach, afterEach);
     free_test_list(tests);
